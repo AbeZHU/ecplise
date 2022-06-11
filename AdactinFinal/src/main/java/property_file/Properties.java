d1d9e5c57b0b2e13503a1fe9b3973f664aff42ef
AbeZHU <nooneforever@gmail.com>
2022-06-11 09:26:20 +0530

    property push

---
+++ AdactinFinal/src/main/java/property_file/Properties.java
@@ -0,0 +1,8 @@
+package property_file;
+
+import java.io.File;
+
+public class Properties {
+	File file = new File("C:\\Users\\noone\\git\\ecplise\\AdactinFinal\\src\\main\\java\\property_file\\Properties.java");
+
+}
